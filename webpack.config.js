f16eb9dbecd709d270dddfae87e84f8e947f370c
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2023-01-09 20:52:31 +0500

    feat: fix

--- webpack.config.js
+++ webpack.config.js
@@ -69,13 +69,13 @@ module.exports = {
       //   test: /\.(png|jpg|jpeg|gif)$/i,
       //   type: 'asset/resource',
       // },
-      {
-        test: /\.svg$/,
-        type: 'asset/resource',
-        generator: {
-          filename: path.join('icons', '[name].[contenthash][ext]'),
-        },
-      },
+      // {
+      //   test: /.svg$/,
+      //   type: 'asset/resource',
+      //   generator: {
+      //     filename: path.join('icons', '[name].[contenthash][ext]'),
+      //   },
+      // },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
